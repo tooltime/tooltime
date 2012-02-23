ea5f3d546858b1e2aa22a505562a1e8d9bea3b99
Marcus Ortiz <mportiz08@gmail.com>
2012-02-22 22:58:50 -0800

    implemented basic species filter for comparisons

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,6 +1,11 @@
 class ComparisonsController < ApplicationController
   def index
-    @comparisons  = Comparison.all
     @species_opts = Comparison.all_species.map {|s| [s, s]}
+    if params[:species].nil?
+      @comparisons = Comparison.all
+    else
+      @selected_species = params[:species]
+      @comparisons = Comparison.where(:species => @selected_species)
+    end
   end
 end
