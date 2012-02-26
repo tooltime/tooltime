7a3f03469f9f96945a07d13a5b27e88f743bca30
Marcus Ortiz <mportiz08@gmail.com>
2012-02-25 16:39:41 -0800

    added identified columns for experiments

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -2,4 +2,14 @@ class Experiment < ActiveRecord::Base
   belongs_to :comparison
   has_many   :genes
   validates_uniqueness_of :label, :scope => :tess
+  
+  def identified(regulation=nil)
+    if regulation.nil?
+      self.genes.size
+    elsif regulation == :up
+      self.genes.select{|g| g.regulation == 'up'}.size
+    elsif regulation == :down
+      self.genes.select{|g| g.regulation == 'down'}.size
+    end
+  end
 end
