42c89587e6efd6875dcb1f3f4d5ef9966f5d736f
Marcus Ortiz <mportiz08@gmail.com>
2012-02-26 12:49:44 -0800

    listing genes for an experiment

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -2,6 +2,9 @@ class Gene < ActiveRecord::Base
   belongs_to :experiment
   has_one :promoter
   has_many :regulatory_elements
-  
   validates_uniqueness_of :name, :scope => :species
+  
+  def num_factors
+    self.regulatory_elements.reduce(0) {|sum, re| sum + re.transcription_factors.size}
+  end
 end
