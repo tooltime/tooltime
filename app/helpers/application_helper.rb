937e2446551cee675167a1cd6f15b28d9ac46d45
Marcus Ortiz <mportiz08@gmail.com>
2012-02-28 21:02:09 -0800

    added helper to give a table header arrow links for sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def sortable_col(col)
+    ("#{col.to_s.titleize.downcase} " +
+    "<span class=\"arrows\" style=\"float: right;\">#{link_to('<i class="icon-chevron-up"></i>'.html_safe, :sort => col.to_sym, :order => 'asc')} " +
+    "#{link_to('<i class="icon-chevron-down"></i>'.html_safe, :sort => col.to_sym, :order => 'desc')}</span>").html_safe
+  end
 end
