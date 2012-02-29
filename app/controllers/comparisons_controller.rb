208c43f239d3150cfc99241f234afa7cdcb75216
Marcus Ortiz <mportiz08@gmail.com>
2012-02-28 22:16:44 -0800

    added sorting for experiments..looks good, but can't test w/o more than 1 experiment...

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -5,12 +5,30 @@ class ComparisonsController < ApplicationController
   end
   
   def show
-    @comparison = Comparison.find(params[:id])
+    @comparison  = Comparison.find(params[:id])
+    @experiments = sorted_experiments(@comparison.experiments)
   end
   
   private
   
   def filtered_comparisons
     params[:species].nil? ? Comparison.all : Comparison.where(species: params[:species])
+  end
+  
+  def sorted_experiments(experiments)
+    if params[:sort] =~ /genes_identified_/ && experiments.size > 1
+      i_by = params[:sort].split('_').last
+      dir = params[:sort].split('_').last.to_sym
+      dir = nil if i_by == 'total'
+      if params[:order] == 'asc'
+        experiments.sort_by {|a,b| a.identified(dir) <=> b.identified(dir)}
+      elsif params[:order] == 'desc'
+        experiments.sort_by {|a,b| b.identified(dir) <=> a.identified(dir)}
+      end
+    elsif params[:sort] =~ /genes_identified_/ && experiments.size <= 1
+      experiments
+    else
+      sorted_collection(experiments)
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def sortable_col(col)
-    ("#{col.to_s.titleize.downcase} " +
+  def sortable_col(col, txt=nil)
+    ("#{txt.nil? ? col.to_s.titleize.downcase : txt} " +
     "<span class=\"arrows\" style=\"float: right;\">#{link_to('<i class="icon-chevron-up"></i>'.html_safe, :sort => col.to_sym, :order => 'asc')} " +
     "#{link_to('<i class="icon-chevron-down"></i>'.html_safe, :sort => col.to_sym, :order => 'desc')}</span>").html_safe
   end
