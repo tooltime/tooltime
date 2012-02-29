c933a0d86efedd0aab38b330a719aa365144455e
Jason Stewart <stewartj89@gmail.com>
2012-02-29 14:38:48 -0800

    added ascending sorting to gene columns

--- app/controllers/genes_controller.rb
+++ app/controllers/genes_controller.rb
@@ -1,5 +1,23 @@
 class GenesController < ApplicationController
   def index
     @genes = Gene.all
+    sort = params[:sort]
+
+    case sort
+    	when "id"
+    		@genes = Gene.order(sort)
+    	when "species"
+    		@genes = Gene.order(sort)
+    	when "name"
+    		@genes = Gene.order(sort)
+    	when "abbrev"
+    		@genes = Gene.order(sort)
+    	when "chromosome"
+    		@genes = Gene.order(sort)
+    	when "beg_site"
+    		@genes = Gene.order(sort)
+    	when "end_site"
+    		@genes = Gene.order(sort)   		
+    end    	
   end
-end
+end 
