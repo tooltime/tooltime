1e3ab03055eb9757348aba3726aea40abcc7f076
Marcus Ortiz <mportiz08@gmail.com>
2012-02-28 22:34:42 -0800

    sorting reg elements for a given gene

--- app/controllers/browse_genes_controller.rb
+++ app/controllers/browse_genes_controller.rb
@@ -1,7 +1,8 @@
 class BrowseGenesController < ApplicationController
   def show
-    @comparison = Comparison.find(params[:comparison_id])
-    @experiment = Experiment.find(params[:experiment_id])
-    @gene       = Gene.find(params[:id])
+    @comparison   = Comparison.find(params[:comparison_id])
+    @experiment   = Experiment.find(params[:experiment_id])
+    @gene         = Gene.find(params[:id])
+    @reg_elements = sorted_collection(@gene.regulatory_elements)
   end
 end
