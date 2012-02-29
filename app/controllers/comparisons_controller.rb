004c27f784f17228237729c87e5a78b572a82133
Marcus Ortiz <mportiz08@gmail.com>
2012-02-28 21:37:33 -0800

    generalized some methods for sorting by column names

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,15 +1,16 @@
 class ComparisonsController < ApplicationController
   def index
     @species_opts = Comparison.all_species.map {|s| [s, s]}
-    if params[:species].nil?
-      @comparisons = Comparison.all
-    else
-      @selected_species = params[:species]
-      @comparisons = Comparison.where(:species => @selected_species)
-    end
+    @comparisons  = sorted_collection(filtered_comparisons)
   end
   
   def show
     @comparison = Comparison.find(params[:id])
+  end
+  
+  private
+  
+  def filtered_comparisons
+    params[:species].nil? ? Comparison.all : Comparison.where(species: params[:species])
   end
 end
