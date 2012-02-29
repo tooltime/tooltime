9c83840cf8fc81047abbfce72b2e1e5baf00d773
Alan Yang <alanyang@sbcglobal.net>
2012-02-28 17:32:33 -0800

    Sortable columns (ASC only)

--- app/models/transcription_factor.rb
+++ app/models/transcription_factor.rb
@@ -2,4 +2,15 @@ class TranscriptionFactor < ActiveRecord::Base
   has_and_belongs_to_many :regulatory_elements
   
   validates_uniqueness_of :name
+
+   #Count the number of unique genes the given transcription factor appears in
+  def count_genes()
+    self.regulatory_elements.map {|reg| reg.gene_id}.to_set.count
+  end
+
+  #Count the total number of times the given transcription factor appears in the DB
+  def count_total()
+    self.regulatory_elements.count
+  end
+
 end
