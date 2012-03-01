8968d696fd6f8eb72c4373f9837c765d04b633d0
Jason Stewart <stewartj89@gmail.com>
2012-02-29 17:59:54 -0800

    Gene sorting ASC/DESC

--- app/controllers/genes_controller.rb
+++ app/controllers/genes_controller.rb
@@ -1,23 +1,5 @@
 class GenesController < ApplicationController
   def index
-    @genes = Gene.all
-    sort = params[:sort]
-
-    case sort
-    	when "id"
-    		@genes = Gene.order(sort)
-    	when "species"
-    		@genes = Gene.order(sort)
-    	when "name"
-    		@genes = Gene.order(sort)
-    	when "abbrev"
-    		@genes = Gene.order(sort)
-    	when "chromosome"
-    		@genes = Gene.order(sort)
-    	when "beg_site"
-    		@genes = Gene.order(sort)
-    	when "end_site"
-    		@genes = Gene.order(sort)   		
-    end    	
+    @genes = sorted_collection(Gene.all)
   end
 end 
