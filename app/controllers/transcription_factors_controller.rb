b5ff6f4b7ad037e997ffa8e1a05a1df3316e1cb0
Alan Yang <alanyang@sbcglobal.net>
2012-03-02 21:17:20 -0800

    sorting both ways and removed id column

--- app/controllers/transcription_factors_controller.rb
+++ app/controllers/transcription_factors_controller.rb
@@ -1,17 +1,18 @@
 class TranscriptionFactorsController < ApplicationController
   def index
     @factors = TranscriptionFactor.all
-    sort = params[:sort]
+    sort = params[:sort].to_s
+    dir  = params[:order].to_s
 
     case sort
       when "id", "name"
-        @factors = TranscriptionFactor.order(sort)
+        @factors = TranscriptionFactor.order("#{sort} #{dir}")
       when "fac"
       when "modelCount"
       when "geneCount"
-        @factors = TranscriptionFactor.joins(:regulatory_elements => :gene).group("transcription_factors.id").order("count(DISTINCT genes.id) asc")
+        @factors = TranscriptionFactor.joins(:regulatory_elements => :gene).group("transcription_factors.id").order("count(DISTINCT genes.id) #{dir}")
       when "totalCount"
-        @factors = TranscriptionFactor.joins(:regulatory_elements).group("transcription_factors.id").order("count(regulatory_elements.id) asc")
+        @factors = TranscriptionFactor.joins(:regulatory_elements).group("transcription_factors.id").order("count(regulatory_elements.id) #{dir}")
       else
     end
   end
@@ -19,17 +20,18 @@ class TranscriptionFactorsController < ApplicationController
   def show
     @factor = TranscriptionFactor.find(params[:id])
     @regulatoryElements = @factor.regulatory_elements
-    sort = params[:sort]
+    sort = params[:sort].to_s
+    dir = params[:order].to_s
 
     case sort 
       when "state"
-        @regulatoryElements = @regulatoryElements.joins(:gene => {:experiment => :comparison}).find(:all, :order => sort)
+        @regulatoryElements = @regulatoryElements.joins(:gene => {:experiment => :comparison}).find(:all, :order => "#{sort} #{dir}")
       when "label", "tess"
-        @regulatoryElements = @regulatoryElements.joins(:gene => :experiment).find(:all, :order => sort)
+        @regulatoryElements = @regulatoryElements.joins(:gene => :experiment).find(:all, :order => "#{sort} #{dir}")
       when "name" 
-        @regulatoryElements = @regulatoryElements.joins(:gene).find(:all, :order => sort)
+        @regulatoryElements = @regulatoryElements.joins(:gene).find(:all, :order => "#{sort} #{dir}")
       when "beg", "model"
-        @regulatoryElements = @regulatoryElements.order(sort)
+        @regulatoryElements = @regulatoryElements.order("#{sort} #{dir}")
       else
     end
   end
