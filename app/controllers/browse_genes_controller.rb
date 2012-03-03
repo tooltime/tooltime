26e2976d756269484a3915efd284c53846e7deb6
Marcus Ortiz <mportiz08@gmail.com>
2012-03-03 15:47:10 -0800

    implemented filtering by la

--- app/controllers/browse_genes_controller.rb
+++ app/controllers/browse_genes_controller.rb
@@ -3,6 +3,19 @@ class BrowseGenesController < ApplicationController
     @comparison   = Comparison.find(params[:comparison_id])
     @experiment   = Experiment.find(params[:experiment_id])
     @gene         = Gene.find(params[:id])
-    @reg_elements = sorted_collection(@gene.regulatory_elements)
+    @reg_elements = sorted_collection(filtered_elements(@gene.regulatory_elements))
+  end
+  
+  private
+  
+  def filtered_elements(elements)
+    if params[:la]
+      elements.where("la >= ?", params[:la])
+    elsif params[:la_low] && params[:la_high]
+      elements.where("la >= :la_low AND la <= :la_high",
+                     {:la_low => params[:la_low], :la_high => params[:la_high]})
+    else
+      elements
+    end
   end
 end
