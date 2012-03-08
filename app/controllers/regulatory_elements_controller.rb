6ee992d3298d7cc7d57aba3b5396762dec7c8ebc
Ben Voogd <bvoogd@calpoly.edu>
2012-03-07 17:58:31 -0800

    added editing functionality for regulatory elements as well as allowed for the viewing of a specific regulatory element in the db summary page

--- app/controllers/regulatory_elements_controller.rb
+++ app/controllers/regulatory_elements_controller.rb
@@ -2,4 +2,19 @@ class RegulatoryElementsController < ApplicationController
    def index
       @regulatory_elements = sorted_collection(RegulatoryElement.all)
    end
+   
+  def show 
+    @regulatory_element = RegulatoryElement.find(params[:id])  
+  end
+  
+  def edit
+    @regulatory_element = RegulatoryElement.find(params[:id])
+  end
+  
+  def update
+    regulatory_element = RegulatoryElement.find(params[:id])
+    regulatory_element.update_attributes(params[:regulatory_element])
+    flash[:notice] = 'Regulatory Element updated successfully.'
+    redirect_to regulatory_element
+  end
 end
