438f43b5698711e57f41f8a7302291acb41a1904
Marcus Ortiz <mportiz08@gmail.com>
2012-03-09 19:45:49 -0800

    implemented l value constraints

--- app/models/gene_search.rb
+++ app/models/gene_search.rb
@@ -35,9 +35,17 @@ class GeneSearch
       if empty_constraints?
         @reg_elements[f.name] = all_elements
       else
-        #@reg_elements[f.name] = f.regulatory_elements.where
         if !@constraints[:la].empty?
           all_elements = all_elements.where('la > :la', {:la => @constraints[:la]})
+        end
+        if !@constraints[:la_slash].empty?
+          all_elements = all_elements.where('la_slash > :la_slash', {:la_slash => @constraints[:la_slash]})
+        end
+        if !@constraints[:lq].empty?
+          all_elements = all_elements.where('lq > :lq', {:lq => @constraints[:lq]})
+        end
+        if !@constraints[:ld].empty?
+          all_elements = all_elements.where('ld < :ld', {:ld => @constraints[:ld]})
         end
         @reg_elements[f.name] = all_elements
       end
